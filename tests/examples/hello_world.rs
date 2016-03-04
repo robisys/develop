ea4a829497a2674cab08cdd6613be954f3dd6f7e
Robert P <robisys@users.noreply.github.com>
2016-03-04 11:38:49 +0100

    Added files via upload

--- tests/examples/hello_world.rs
+++ tests/examples/hello_world.rs
@@ -1,5 +1,17 @@
-//#[macro_use] extern crate nickel;
+use util::{run_example, read_url};
 
-fn  main() {
-   println!("Hello World");
+fn t(file: &str) {
+    run_example(file, |port| {
+        let paths = ["", "foo", "bar.html", "foo-barrrr/baz"];
+
+        for path in &paths {
+            let url = format!("http://localhost:{}/{}", port, path);
+            let s = read_url(&url);
+
+            assert_eq!(s, "Hello World");
+        }
+    })
 }
+
+#[test] fn non_macro() { t("hello_world") }
+#[test] fn _macro() { t("hello_world_macro") }
