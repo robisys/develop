535d613b57ab5b9f2b94387496284adf3ba17e49
Robert P <robisys@users.noreply.github.com>
2016-03-08 20:50:23 +0100

    Added files via upload

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+//! Show and Edit Cargo's Manifest Files
+
+#![cfg_attr(test, allow(dead_code))]
+#![deny(missing_docs, missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts, unsafe_code, unstable_features, unused_import_braces, unused_qualifications)]
+#![cfg_attr(feature = "dev", allow(unstable_features))]
+#![cfg_attr(feature = "dev", feature(plugin))]
+#![cfg_attr(feature = "dev", plugin(clippy))]
+
+#[macro_use]
+extern crate quick_error;
+extern crate toml;
+
+mod manifest;
+mod dependency;
+
+pub use dependency::Dependency;
+pub use manifest::Manifest;
