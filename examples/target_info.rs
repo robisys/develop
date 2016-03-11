40a36d87148e05cf11501e9af2a9cc248f728349
Robert P <robisys@users.noreply.github.com>
2016-03-11 11:59:12 +0100

    Update target_info.rs

--- examples/target_info.rs
+++ examples/target_info.rs
@@ -1,10 +1,5 @@
-# target_info
 
-Super simple library to get stringified versions of the various build target configuration attributes.
-
-Example:
-
-```rust
 use target_info::Target;
 println!("Target is {}-{}-{}", Target::arch(), Target::env(), Target::os());
-```
+
+
