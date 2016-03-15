19fed54574b49d5ae005ff8c648085ef42a4e20a
Robert P <robisys@users.noreply.github.com>
2016-03-15 17:36:46 +0100

    Added files via upload

--- src/myprog.rs
+++ src/myprog.rs
@@ -1,6 +1,43 @@
+//  myprog.rs
+#![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
+       html_favicon_url = "https://doc.rust-lang.org/favicon.ico",
+       html_root_url = "https://doc.rust-lang.org/nightly/",
+       html_playground_url = "https://play.rust-lang.org/",
+       issue_tracker_base_url = "https://github.com/rust-lang/rust/issues/",
+       test(no_crate_inject, attr(deny(warnings))),
+       test(attr(allow(dead_code, deprecated, unused_variables, unused_mut))))]
+// 
+//! this file: myprog.rs
+//!
+//! Show and Edit Cargo's Manifest Files
+//!
+//!
+//! # Example 1
+//!
+//!```rust
+//!  fn mydoc () {};
+//!
+//!  mydoc();
+//!
+//!
+//!```
+//! # Example 2
+//!
+//!```
+//! fn main() {
+//!
+//!
+//!
+//!
+//!
+//!    }
+//!```
+//!
+
 //
 //
 use std::env;
+//use rpdoc;
 
 #[cfg(unix)]
 fn is_executable() {
@@ -10,8 +47,11 @@ fn is_executable() {
 fn is_executable() {
    println!("is windows");
    }   
-   
+/// # pargs
+/// hier diese Funktion   
 pub fn pargs() {
+//! # pargs inhalt
+//!  weitere Info
     for (key,value) in env::vars_os() {
     println!("{:?}: {:?}", key, value);
     }
@@ -24,6 +64,54 @@ is_executable();
 println!("Hallo World");
 }
 
+
+/// # rpdoc1 start
+///   erklaerung
+//
+pub fn  rpdoc1() {
+//! # rpdoc1 
+//! innerhalb
+
+println!("Hallo World rpdoc ");
+
+}  
+
+
+
+
+
+
+pub fn zatest() {
+//! # zatest
+//! 
+//!  it reall_works
+//!
+//! # Example
+//!
+//!```
+//! // #[macro_use]
+//! //extern crate assert_cli;
+//! 
+//! //assert_cli!("g ",&["list", "--dev","--"] => Success,r#"    "#).unwrap();
+//!
+//! println! ("eine Bemerkung");
+//! 
+//! //parg() ;
+//! fn Ztest()  { 
+//!
+//! }
+//!
+//! //rpdoc1 ();
+//!
+//! Ztest();
+//!```
+//!
+ println! ("eine Bemerkung zatest");
+
+
+}
+
 #[test]
 fn its_really_works() {
+  zatest();
 }
