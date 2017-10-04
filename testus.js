5f5800b4854719eb14c2d038e72971e6617e3146
Robert P <robisys@users.noreply.github.com>
2017-10-04 13:51:20 +0200

    Update testus.js

--- testus.js
+++ testus.js
@@ -2,6 +2,7 @@ console.log("hallo");
 /*
 href="https://github.com/wireapp/don-bot
  neue zeile  
+ 
 Hi, I'm GitHub-Bot. Here is how to set me up:
 
 1. Go to the repository that you want to connect to
