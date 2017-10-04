03ca08cb126caa8caa2f6f87f9f0061b846025cc
Robert P <robisys@users.noreply.github.com>
2017-10-04 10:45:51 +0200

    Update testus.js

--- testus.js
+++ testus.js
@@ -1,1 +1,12 @@
 console.log("hallo");
+/*
+Hi, I'm GitHub-Bot. Here is how to set me up:
+
+1. Go to the repository that you want to connect to
+2. Go to Settings / Webhooks / Add webhook
+3. Add Payload URL: https://35.187.99.125/github/e7776d0a-55f4-4997-8816-651d61137130
+4. Set Content-Type: application/json
+5. Disable SSL verification
+6. Set Secret: hl17ak
+
+*/
