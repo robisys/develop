edf62fd4fc186ba353f0432f6a3110c86b1dd010
Robert P <robisys@users.noreply.github.com>
2017-10-05 15:41:33 +0200

    Update wire-run.sh

--- wire-run.sh
+++ wire-run.sh
@@ -1,4 +1,16 @@
 
+#  Create service key and certificate:
+
+openssl genrsa -out server.key 4096
+openssl req -new -key server.key -out csr.pem
+openssl x509 -req -days 7300 -in csr.pem -signkey server.key -out server.crt
+openssl rsa -in server.key -pubout -out pubkey.pem
+
+
+
+
+
+
 ssh-keyscan -t rsa wirebot.robisys.de | sed "s/^[^ ]* //"  > wirebot1.pub 
 
 #ssh-keyscan -t rsa wirebot.robisys.de 
